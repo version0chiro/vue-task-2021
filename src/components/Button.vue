d5cc35d3c63b2bbdbabd3ce107a656629024822a
Chiro <sachinsinghroxx@gmail.com>
2021-10-01 01:02:38 +0530

    started working on Vue

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button :style="{background:color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'Add Task',
+
+        }, color: {
+            type: String,
+            default: 'green'
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @click="onClick()" text="Add Task" color="green" />
+        <!-- <Button text="Update Task" color="blue"/> -->
+        <!-- <Button text="Delete Task" color="red"/> -->
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'Welcome to Vue.js'
+        }
+    }, components: {
+        Button
+    }, methods: {
+        onClick() {
+            console.log("add");
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
